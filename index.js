6af2c267315061c30a5c467d16ac604a11852510
fbansept <bansept.franck@gmail.com>
2025-05-05 14:23:40 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+const bcrypt = require("bcrypt");
+const jwtUtil = require("jsonwebtoken");
+const app = express();
+
+// Configuration de la base de données
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "project-dw1",
+});
+
+// Connexion à la base de données
+connection.connect((err) => {
+  if (err) {
+    console.error("Erreur de connexion à la base de données :", err);
+    return;
+  }
+  console.log("Connecté à la base de données MySQL");
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.post("/inscription", (req, res) => {
+  const utilisateur = req.body;
+
+  bcrypt.hash(utilisateur.password, 10, (err, hash) => {
+    connection.query(
+      "INSERT INTO utilisateur(email, password) VALUES (?,?)",
+      [utilisateur.email, hash],
+      (err, resultat) => {
+        if (err) {
+          console.debug(err);
+          return res.sendStatus(500);
+        }
+
+        res.json({ message: "utilsateur enregistré" });
+      }
+    );
+  });
+});
+
+app.post("/connexion", (req, res) => {
+  const utilisateur = req.body
+
+  connection.query(
+    "SELECT * FROM utilisateur WHERE email = ?",
+    [utilisateur.email],
+    (err, resultat) => {
+      if (err) {
+        console.debug(err);
+        return res.sendStatus(500);
+      }
+
+      if(resultat.length != 1) {
+        return res.sendStatus(401);
+      }
+
+      bcrypt.compare(utilisateur.password, resultat[0].password, (err, compatible) => {
+        if (err) {
+          console.debug(err);
+          return res.sendStatus(500);
+        }
+
+        if (compatible) {
+          return res.json({ message: "vous etes connecté" });
+        }
+
+        return res.sendStatus(401);
+        
+      });
+    }
+  );
+})
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
